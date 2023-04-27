e6d33358d6cb072ef623d1b03c8adb6886f00039
Umavengalaraju <umavengalaraju20@gmail.com>
2023-04-27 14:58:58 +0530

    uma

--- src/app/blackgalaxy/blackgalaxy.page.ts
+++ src/app/blackgalaxy/blackgalaxy.page.ts
@@ -30,7 +30,9 @@ export class BlackgalaxyPage implements OnInit {
     // cart1: any=[]
     filter: any;
     type:any;
-  
+  itemsCart: any;
+  cartItem:number = 0;
+  data: any;
   
   constructor(){
     this.get();
@@ -46,10 +48,68 @@ export class BlackgalaxyPage implements OnInit {
      
       this.get();
       
+    const localdata=localStorage.getItem('anu')
+    if(localdata!=null){
+      this.data = JSON.parse(localdata)
+    }
+    this.cartItemFunc();
+      
     
     }
   
+    addCart(category: any) {
+      let cartDataNull = localStorage.getItem('anunya'); // cartDataNull is a variable , localCart is a key,
+      if (cartDataNull == null) {
+        let storeDataGet: any = []; // storeDataGet is a array
+        storeDataGet.push(category); // push the category into localstorage
+        localStorage.setItem('anunya',JSON.stringify(storeDataGet));
+       
+       
+    //     Swal.fire('Added Successfully!', '', 'success').then(() => {
+    //       window.location.reload()
+    //    } );
+       //localCart is key ,storeDataGet is To convert the stringify
+      }
+      
+      else {
+        var id = category.prodId;  // this is product id represented
+        let index: number = -1;
+        this.itemsCart = JSON.parse(localStorage.getItem('anunya') || '{}'); // 
+        for (let i = 0; i < this.itemsCart.length; i++) {   // i is a loop vice (inc is the products)
+          if (parseInt(id) === parseInt(this.itemsCart[i].prodId)) {    // In parseInt to store itemcart array in index
+            this.itemsCart[i].qnt = category.qnt;  // this qnt is inc and dec the product
+            index = i;
+            break;  // to break the function (end)
+          }
+        }
+     
+        if (index == -1) {
+          this.itemsCart.push(category);  // this line product item to push itemcart array
+          localStorage.setItem('anunya', JSON.stringify(this.itemsCart));  //
+        }
+        else {
+          localStorage.setItem('anunya', JSON.stringify(this.itemsCart));
+        }
+      
+  
     
+      }
+      // Swal.fire('Added to Cart!', '', 'success').then(() =>
+      //  {
+      //   window .location.reload()
+      // });
+      // window .location.reload()
+     
+        
+  }
+  cartItemFunc(){
+    if(localStorage.getItem('anunya') != null){
+    var cartCount = JSON.parse(localStorage.getItem('anunya') || '{}');
+    this.cartItem = cartCount.length;
+    console.log(this.cartItem)
+  
+  }
+  }
   
      
     
